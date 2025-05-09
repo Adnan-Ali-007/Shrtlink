ea73496cbce15bc9bdbda95e90fc4820875abfdc
Harshit Rai <123428280+heyharshitrai@users.noreply.github.com>
2025-05-09 23:47:36 +0530

    Update app.py
    Dynamically switching behaviour based on environment

--- app.py
+++ app.py
@@ -40,4 +40,8 @@ def redirect_slug(slug):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    if os.environ.get("RENDER") == "true":
+        port = int(os.environ.get("PORT", 10000))
+        app.run(host='0.0.0.0', port=port)
+    else:
+        app.run(debug=True)
